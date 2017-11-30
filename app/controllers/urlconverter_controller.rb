648ab546c64166f18a09fe58b2a463a871c6fe1c
Ban Sang Min <tkdals1119@gmail.com>
2017-11-30 07:58:06 +0000

    code arrange, add remark

--- app/controllers/urlconverter_controller.rb
+++ app/controllers/urlconverter_controller.rb
@@ -2,11 +2,11 @@ class UrlconverterController < ApplicationController
 
   
     
-  def create
+  def create # originurl 과 shorturl 을 받아와서 중복 검사를 한 뒤 DB에 추가하는 함수입니다.
     @originurl = params[:originurl] 
     @shorturl = params[:shorturl]
     
-    if Converter.exists?(originurl: @originurl)
+    if Converter.exists?(originurl: @originurl) # 입력 받은 originurl 의 중복검사를 하는 부분입니다.
         @already_short_url = Converter.find_by(originurl: @originurl)
         render json: @already_short_url
     else
