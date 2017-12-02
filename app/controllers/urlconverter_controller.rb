68f506922671f6dfa9fe3b5b786ea2e65ca0e612
Ban Sang Min <tkdals1119@gmail.com>
2017-12-02 08:52:27 +0000

    change template, make url expression

--- app/controllers/urlconverter_controller.rb
+++ app/controllers/urlconverter_controller.rb
@@ -1,7 +1,5 @@
 class UrlconverterController < ApplicationController
 
-  
-    
   def create # originurl 과 shorturl 을 받아와서 중복 검사를 한 뒤 DB에 추가하는 함수입니다.
     @originurl = params[:originurl] 
     @shorturl = params[:shorturl]
@@ -18,5 +16,14 @@ class UrlconverterController < ApplicationController
   
   def index
   end
-
+  
+  def convert
+    @getshort = params[:shorturl]
+    @sendshort = "https://urishortening-db-sangm2n.c9users.io/" + @getshort
+    
+    @getorigin = Converter.find_by(shorturl: @sendshort)
+    
+    puts @getorigin.originurl
+    redirect_to @getorigin.originurl
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   
   get '/create' => 'urlconverter#create'
   get '/index' => 'urlconverter#index'
+  get '/:shorturl' => 'urlconverter#convert'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
